ed4c12f68591af9d67235e053c0df0ee0b02a0e0
Caleb Harrison <calebbharrison@icloud.com>
2026-02-03 12:42:04 -0600

    update

--- my/util/DLinkedList.java
+++ my/util/DLinkedList.java
@@ -4,5 +4,16 @@ public class DLinkedList<T> {
 
     private DNode<T> head;
     private DNode<T> tail;
-    
+
+    public DLinkedList() {
+        
+    }
+
+    public void addAfter(DNode<T> node, T data) {
+    }
+
+    public void add(T data) {
+
+    }
+
 }
--- my/util/DNode.java
+++ my/util/DNode.java
@@ -2,4 +2,39 @@ package my.util;
 
 public class DNode<T> {
 
+    public T data;
+    public DNode<T> prev;
+    public DNode<T> next;
+
+    public DNode(T data) {
+        this.prev = null;
+        this.next = null;
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public DNode<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(DNode<T> prev) {
+        this.prev = prev;
+    }
+
+    public DNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(DNode<T> next) {
+        this.next = next;
+    }
+
+    
 }
